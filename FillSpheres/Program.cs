e352124d24c0845ae57015fb0f1edb48505e5268
Mikapuccino <nelsonfcmilheiro@gmail.com>
2023-04-26 15:46:43 +0100

    Test Color class, program works properly

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -7,6 +7,14 @@ namespace FillSpheres
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Color color = new Color(200, 10, 150);
+
+            Console.WriteLine(color.GetRed());
+            Console.WriteLine(color.GetGreen());
+            Console.WriteLine(color.GetBlue());
+            Console.WriteLine(color.GetAlpha());
+            Console.WriteLine(color.GetGrey());
         }
     }
 }
